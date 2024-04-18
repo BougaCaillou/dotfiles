99b5334313589cb48b3dfc1793c9fd828a865f49
Bouga <braickaimouth@gmail.com>
2024-04-18 15:09:15 +0200

    feat(omz): add zsh-autosuggestions plugin

---
+++ omz/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
@@ -0,0 +1,5 @@
+# This file exists because my configuration enables this plugin.
+# Since I don't want to remove it from my rc file, this file is here to
+# prevent errors.
+#
+# If you want to use zsh syntax highlighting, check the install script notes.
