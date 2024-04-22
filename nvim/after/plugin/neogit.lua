42eb0d39cf0d6f342143c7ab5d1ef98910e8fcf0
Bouga <braickaimouth@gmail.com>
2024-04-22 11:05:28 +0200

    feat: add diffview keymap for neogit diff integration
    The diffview got fixed some time ago and it is now usable.

--- nvim/after/plugin/neogit.lua
+++ nvim/after/plugin/neogit.lua
@@ -7,4 +7,9 @@ neogit.setup {
   }
 }
 
+-- Open Neogit panel
 vim.keymap.set('n', '<leader>gs', '<cmd>Neogit kind=split<cr><C-w>J')
+
+-- Open and close Diffview panel
+vim.keymap.set('n', '<leader>gd', '<cmd>DiffviewOpen<cr>')
+vim.keymap.set('n', '<leader>gc', '<cmd>DiffviewClose<cr>')
