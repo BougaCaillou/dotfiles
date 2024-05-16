c8abcd94ad5b5926f4b664b8ec5e795475cd55cc
Bouga <braickaimouth@gmail.com>
2024-05-16 18:19:21 +0200

    feat: switch to brew path for custom plugins

--- omz/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+++ omz/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
@@ -1,5 +1,8 @@
 # This file exists because my configuration enables this plugin.
 # Since I don't want to remove it from my rc file, this file is here to
 # prevent errors.
-#
-# If you want to use zsh syntax highlighting, check the install script notes.
+
+if [ -f $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]; then
+  source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+fi
+
