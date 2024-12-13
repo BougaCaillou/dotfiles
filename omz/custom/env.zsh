746623534d87bce707f3ea5d6285ff303c1b1e26
BougaCaillou <brakckaimouth@gmail.com>
2024-12-13 17:22:44 +0100

    chore(omz): removed useless comments

--- omz/custom/env.zsh
+++ omz/custom/env.zsh
@@ -1,13 +1,10 @@
-# PATH
 PATH="/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin";
 
-# Dotfiles home directory
 export DOTFILES="$HOME/repos/dotfiles"
 
 # Oh you got me some BINs
 PATH="$HOME/bin:$HOME/.yarn/bin:$HOME/repos/REDACTED_TOOLING/bin:$HOME/python-venv/bin:$PATH";
 
-# ADD Ruby
 PATH="/usr/local/opt/ruby/bin:$PATH"
 export LDFLAGS="-L/usr/local/opt/ruby/lib"
 export CPPFLAGS="-I/usr/local/opt/ruby/include"
@@ -21,12 +18,10 @@ export REDACTED_WIDGET_URL=http://localhost:8080
 # User management env var
 export APP_STORE_ENVIRONMENT="sandbox"
 
-# Android env var
 export ANDROID_HOME="$HOME/Library/Android/sdk"
 export ANDROID_SDK_ROOT=$ANDROID_HOME
 PATH="$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/build-tools:$ANDROID_HOME/platform-tools:$PATH";
 
-# Java home
 export JAVA_ROOT=/Library/Java/JavaVirtualMachines
 
 # libpq (postgre dump  and restore utilities)
@@ -35,35 +30,25 @@ PATH="/opt/homebrew/opt/postgresql@17/bin:$PATH"
 # Postgresql env vars
 export PGPWDL='mysecretpassword'
 
-# Rust utilities (cargo etc)
 PATH="$HOME/.cargo/bin:$PATH"
 
 # Custom tools (one ofs, i.e. Mermerd...)
 PATH="$HOME/custom-tools:$PATH"
 
-# REDACTED_TOOLING bin
 PATH="$HOME/repos/REDACTED_TOOLING/bin:$PATH"
 
-# Local bin directory
 PATH="$HOME/.local/bin:$PATH"
 
-# TMUX home directory
 export TMUX_HOME="$HOME/.config/tmux"
 
-# Main projects directory (where all the projects are cloned)
 export PROJECTS_DIR="$HOME/repos"
 
-# Setting XDG directories
 export XDG_CONFIG_HOME="$HOME/.config"
 
-# Adding stuff for nix darwin
 PATH="/run/current-system/sw/bin:$PATH"
-
-# Nix darwin flake path
 export NIX_CONF_DIR="$DOTFILES/nix-darwin"
 PATH="/nix/var/nix/profiles/default/bin:$PATH"
 
-# asdf stuff
 export ASDF_NODEJS_LEGACY_FILE_DYNAMIC_STRATEGY=latest_installed
 
 PATH="$HOME/.meteor:$PATH"
