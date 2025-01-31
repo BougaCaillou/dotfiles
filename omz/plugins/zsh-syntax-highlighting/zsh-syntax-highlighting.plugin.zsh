8a862ed654d2556bd91c73442287992133fd6d10
BougaCaillou <brakckaimouth@gmail.com>
2025-01-31 18:04:49 +0100

    chore(omz): remove now useless zsh plugin files
    Those are installed with brew and sourced directly in .zshrc file, so
    no link is needed for them.

--- omz/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+++
@@ -1,5 +0,0 @@
-# This plugin was installed with homebrew, so I just source it from the homebrew path.
-if [ -f $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]; then
-  source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-fi
-
