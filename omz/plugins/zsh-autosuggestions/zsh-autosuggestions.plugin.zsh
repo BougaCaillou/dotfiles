e07b1916b2a515b5fe28cde4c61713bd2816ab03
BougaCaillou <brakckaimouth@gmail.com>
2025-01-31 18:04:49 +0100

    chore(omz): remove now useless zsh plugin files
    Those are installed with brew and sourced directly in .zshrc file, so
    no link is needed for them.

--- omz/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+++
@@ -1,5 +0,0 @@
-# This plugin was installed with homebrew, so I just source it from the homebrew path.
-if [ -f $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]; then
-  source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-fi
-
