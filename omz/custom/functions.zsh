4dcfcf611a2cbd4afeaa5f4aa2da47e65f89e554
BougaCaillou <brakckaimouth@gmail.com>
2025-05-22 00:30:38 +0200

    feat(nix): cleaned and updated nix
    Updated nix to latest version, had to clean home manager stuff in the
    process (home manager was never used anyway). Rebuilding now requires
    sudo as nix stuff is run by the current user, not root... something like
    that.

--- omz/custom/functions.zsh
+++ omz/custom/functions.zsh
@@ -237,7 +237,7 @@ v() {
 
 # Rebuild nix profile based on my flake
 rebuild() {
-  darwin-rebuild switch --flake $DOTFILES/nix-darwin
+  sudo darwin-rebuild switch --flake $DOTFILES/nix-darwin
   exec zsh
 }
 
