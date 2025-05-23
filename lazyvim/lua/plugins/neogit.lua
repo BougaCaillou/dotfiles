c0f13bdbd3c2e6d5ab95dff7acaba7b28c9e1317
BougaCaillou <brakckaimouth@gmail.com>
2025-05-23 10:33:50 +0200

    fix(lazyvim): neogit loading
    Neogit needs to not be lazy loaded of it can fail to open if <leader>gs
    is used to early when opening nvim

--- lazyvim/lua/plugins/neogit.lua
+++ lazyvim/lua/plugins/neogit.lua
@@ -1,14 +1,15 @@
 return {
   "NeogitOrg/neogit",
+  lazy = false,
   dependencies = {
     "nvim-lua/plenary.nvim", -- required
     "sindrets/diffview.nvim", -- optional - Diff integration
 
     -- Only one of these is needed.
-    "nvim-telescope/telescope.nvim", -- optional
+    -- "nvim-telescope/telescope.nvim", -- optional
     -- "ibhagwan/fzf-lua",              -- optional
     -- "echasnovski/mini.pick", -- optional
-    -- "folke/snacks.nvim",             -- optional
+    -- "folke/snacks.nvim", -- optional
   },
   opts = {
     integrations = {
