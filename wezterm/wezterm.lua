1e8e9eb866059a3d28e9d3ab3f5d55f9ef68e3d3
BougaCaillou <brakckaimouth@gmail.com>
2026-02-19 18:15:46 +0100

    feat(wezterm): set font to Maple Mono NF

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,14 +1,14 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = "Catppuccin Mocha"
 config.window_background_opacity = 0.85
 
 config.window_padding = {
-  left = 7,
-  right = 7,
-  top = 7,
-  bottom = 7,
+	left = 7,
+	right = 7,
+	top = 7,
+	bottom = 7,
 }
 
 -- Now using tmux, there will be only one tab
@@ -16,12 +16,15 @@ config.hide_tab_bar_if_only_one_tab = true
 
 -- Changing some keybindings
 config.keys = {
-  { key = 'w', mods = 'SUPER', action = wezterm.action.DisableDefaultAssignment },
-  { key = 't', mods = 'SUPER', action = wezterm.action.DisableDefaultAssignment },
-  { key = '+', mods = 'SUPER', action = wezterm.action.IncreaseFontSize },
-  { key = '-', mods = 'SUPER', action = wezterm.action.DecreaseFontSize },
-  { key = 'o', mods = 'SUPER', action = wezterm.action.ResetFontSize },
+	{ key = "w", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "t", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "+", mods = "SUPER", action = wezterm.action.IncreaseFontSize },
+	{ key = "-", mods = "SUPER", action = wezterm.action.DecreaseFontSize },
+	{ key = "o", mods = "SUPER", action = wezterm.action.ResetFontSize },
 }
 
-return config
+config.font = wezterm.font_with_fallback({
+	"Maple Mono NF",
+})
 
+return config
